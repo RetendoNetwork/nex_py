73f69f9d2a8729363f7c49ebf60e7893e87fc920
00cedke <mathiskera2509@gmail.com>
2024-11-20 18:06:44 +0100

    Added uint64 type.

--- README.md
+++ README.md
@@ -30,6 +30,7 @@
 - [x] RMC
   - [x] RMC Message
   - [x] RMC Request
+- [x] NAT Checker (Support Add)
 
 # Example for use NEX
 ```py
--- init.py
+++ init.py
@@ -3,11 +3,14 @@ from result_codes import init_result_codes
 from nex_types import variant
 from nex_types.datetime import DateTime
 from nex_types.bool import Bool
+from nex_types.uint64 import UInt64
 
 nexlogger = logger.NewLogger()
+
 
 def init():
     init_result_codes()
     
     variant.register_variant_type(1, Bool(False))
     variant.register_variant_type(2, DateTime(0))
+    variant.register_variant_type(3, UInt64(0))
