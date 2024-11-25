df53314d2d981a26347eab7c1ff22ed565c16b43
00cedke <mathiskera2509@gmail.com>
2024-11-25 20:26:57 +0100

    Fix error from RMC.

--- rmc.py
+++ rmc.py
@@ -3,6 +3,8 @@ from typing import Optional
 from nex.byte_stream_in import ByteStreamIn
 from nex.byte_stream_out import ByteStreamOut
 from nex.endpoint_interface import EndpointInterface
+from nex.nex_types.class_version_container import ClassVersionContainer
+from nex.nex_types.string import String
 
 
 class RMC:
@@ -17,7 +19,7 @@ class RMC:
         self.method_id = 0
         self.method_name = ""
         self.error_code = 0
-        self.version_container = None
+        self.version_container = ClassVersionContainer()
         self.parameters = b''
 
     def copy(self):
@@ -82,18 +84,18 @@ class RMC:
             if stream.remaining() != length:
                 raise ValueError("RMC Message has unexpected size")
 
-            self.protocol_name = stream.read_string()
+            self.protocol_name = String("")
             self.is_request = stream.read_bool()
             if self.is_request:
                 self.call_id = stream.read_uint32_le()
-                self.method_name = stream.read_string()
-                self.version_container = stream.read_class_version_container()
+                self.method_name = String("")
+                self.version_container = ClassVersionContainer.new_class_version_container()
                 self.parameters = stream.read_remaining()
             else:
                 self.is_success = stream.read_bool()
                 if self.is_success:
                     self.call_id = stream.read_uint32_le()
-                    self.method_name = stream.read_string()
+                    self.method_name = String("")
                     self.parameters = stream.read_remaining()
                 else:
                     self.error_code = stream.read_uint32_le()
@@ -112,7 +114,7 @@ class RMC:
     def encode_packed(self) -> bytes:
         stream = ByteStreamOut(self.endpoint.library_versions(), self.endpoint.byte_stream_settings())
 
-        protocol_id_flag = 0x80 if self.is_request else 0
+        protocol_id_flag = 0x80 if self.is_request else protocol_id_flag = 0
 
         if not self.is_hpp or (self.is_hpp and self.is_request):
             if self.protocol_id < 0x80:
@@ -124,15 +126,15 @@ class RMC:
         if self.is_request:
             stream.write_uint32_le(self.call_id)
             stream.write_uint32_le(self.method_id)
-            if self.parameters:
-                stream.write_bytes(self.parameters)
+            if self.parameters != None & len(self.parameters):
+                stream.write_bytes_next(self.parameters)
         else:
             stream.write_bool(self.is_success)
             if self.is_success:
                 stream.write_uint32_le(self.call_id)
                 stream.write_uint32_le(self.method_id | 0x8000)
-                if self.parameters:
-                    stream.write_bytes(self.parameters)
+                if self.parameters != None & len(self.parameters):
+                    stream.write_bytes_next(self.parameters)
             else:
                 stream.write_uint32_le(self.error_code)
                 stream.write_uint32_le(self.call_id)
@@ -141,31 +143,31 @@ class RMC:
 
         message = ByteStreamOut(self.endpoint.library_versions(), self.endpoint.byte_stream_settings())
         message.write_uint32_le(len(serialized))
-        message.write_bytes(serialized)
+        message.write_bytes_next(serialized)
 
         return message.bytes()
 
     def encode_verbose(self) -> bytes:
         stream = ByteStreamOut(self.endpoint.library_versions(), self.endpoint.byte_stream_settings())
 
-        stream.write_string(self.protocol_name)
+        self.protocol_name.write_to(stream)
         stream.write_bool(self.is_request)
         if self.is_request:
             stream.write_uint32_le(self.call_id)
-            stream.write_string(self.method_name)
+            self.method_name.write_to(stream)
             if self.version_container:
-                stream.write_class_version_container(self.version_container)
+                self.method_name.write_to(stream)
             else:
-                stream.write_uint32_le(0)  # Fail safe
+                stream.write_uint32_le(0)
             if self.parameters:
-                stream.write_bytes(self.parameters)
+                stream.write_bytes_next(self.parameters)
         else:
             stream.write_bool(self.is_success)
             if self.is_success:
                 stream.write_uint32_le(self.call_id)
-                stream.write_string(self.method_name)
+                self.method_name.write_to(stream)
                 if self.parameters:
-                    stream.write_bytes(self.parameters)
+                    stream.write_bytes_next(self.parameters)
             else:
                 stream.write_uint32_le(self.error_code)
                 stream.write_uint32_le(self.call_id)
@@ -174,26 +176,26 @@ class RMC:
 
         message = ByteStreamOut(self.endpoint.byte_stream_settings())
         message.write_uint32_le(len(serialized))
-        message.write_bytes(serialized)
+        message.write_bytes_next(serialized)
 
         return message.bytes()
 
-    def new_rmc_message(endpoint):
+    def new_rmc_message(endpoint: EndpointInterface):
         return RMC(endpoint)
 
-    def new_rmc_request(endpoint):
+    def new_rmc_request(endpoint: EndpointInterface):
         msg = RMC(endpoint)
         msg.is_request = True
         return msg
 
-    def new_rmc_success(endpoint, parameters):
+    def new_rmc_success(endpoint: EndpointInterface, parameters):
         msg = RMC(endpoint)
         msg.is_request = False
         msg.is_success = True
         msg.parameters = parameters
         return msg
 
-    def new_rmc_error(endpoint, error_code):
+    def new_rmc_error(endpoint: EndpointInterface, error_code):
         if error_code & 0x8000 == 0:
             error_code |= 0x8000
         msg = RMC(endpoint)
