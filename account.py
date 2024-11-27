c1d2cda755b574a98ae3d3fa56670f1036728924
00cedke <mathiskera2509@gmail.com>
2024-11-27 17:30:15 +0100

    [+] Remove str.

--- account.py
+++ account.py
@@ -2,10 +2,10 @@ from nex.nex_types.pid import PID
 
 
 class Account:
-    def __init__(self, pid: PID, username: str, password: str) -> None:
+    def __init__(self, pid: PID, username, password) -> None:
         self.PID = pid
         self.Username = username
         self.Password = password
 
-    def new_account(pid: PID, username: str, password: str) -> 'Account':
+    def new_account(pid: PID, username, password) -> 'Account':
         return Account(pid, username, password)
