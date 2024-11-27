9f3713a4c8beab0440152e98c2b6684eb5110d19
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-27 18:49:01 +0100

    Delete byte_stream_settings.py

--- byte_stream_settings.py
+++
@@ -1,9 +0,0 @@
-class ByteStreamSettings:
-    def __init__(self, string_length_size=2, pid_size=4, use_structure_header=False):
-        self.string_length_size = string_length_size
-        self.pid_size = pid_size
-        self.use_structure_header = use_structure_header
-
-    @staticmethod
-    def new_byte_stream_settings():
-        return ByteStreamSettings()
