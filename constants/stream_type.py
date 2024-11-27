f2cb1b0d8fe2c8bcaa3bccb5b53cb387db96bb29
00cedke <mathiskera2509@gmail.com>
2024-11-27 17:33:56 +0100

    [+] Modified StreamType.

--- constants/stream_type.py
+++ constants/stream_type.py
@@ -5,15 +5,15 @@ from enum import IntEnum, unique
 class StreamType(IntEnum):
     DO = 1
     RV = 2
-    OldRVSec = 3
+    OlD_RV_SEC = 3
     SBMGMT = 4
     NAT = 5
-    SessionDiscovery = 6
-    NATEcho = 7
-    Routing = 8
-    Game = 9
-    RVSecure = 10
-    Relay = 11
+    SESSION_DISCOVERY = 6
+    NAT_ECHO = 7
+    ROUTING = 8
+    GAME = 9
+    RV_SECURE = 10
+    RELAY = 11
 
     def enum_index(self) -> int:
         return self.value
