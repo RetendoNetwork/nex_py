5a7b9a46398954abccd8adce748bdea6790ac382
00cedke <mathiskera2509@gmail.com>
2024-11-27 17:38:50 +0100

    [+] Added ByteStream import.

--- endpoint_interface.py
+++ endpoint_interface.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 from nex.packet_interface import PacketInterface
 from nex.library_version import LibraryVersions
-from nex.byte_stream_settings import ByteStreamSettings
+from nex.byte_stream import ByteStreamSettings
 from nex.error import Error
 
 
